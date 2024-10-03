84912ab076e7e0dbd1050b3d8c729c4aca0a015f
vesko <ggrrrr@gmail.com>
2024-10-04 00:21:14 +0300

    fixes

---
+++ be/help/tests.go
@@ -0,0 +1,18 @@
+package help
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+func RepoDir() string {
+	return dir()
+}
+func dir() string {
+	_, filename, _, _ := runtime.Caller(1)
+	files := strings.Split(filename, "/")
+	fmt.Printf("help/tests.go: %v\n", filename)
+	rootDir := strings.Join(files[:len(files)-3], "/")
+	return rootDir
+}
